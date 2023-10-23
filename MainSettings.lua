5a4714134a0784307d6106b724d3739719adfcd4
OLDodin <odin_sasa@mail.ru>
2023-10-23 12:09:12 +0300

    Отказ от API, помеченного как deprecated

--- MainSettings.lua
+++ MainSettings.lua
@@ -90,7 +90,7 @@ end
 
 function GetCurrentSwitchIndex(aWdg)
 	local txtWdg = getChild(aWdg, "ModeNameTextView")
-	local currValue = common.ExtractWStringFromValuedText(txtWdg:GetValuedText())
+	local currValue = txtWdg:GetValuedText():ToWString()
 	return GetSwitchIndexByName(m_actionSwitch, currValue)
 end
 
