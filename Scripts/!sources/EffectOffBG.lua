e13484103084e605559c8997cfd79f5c60a0581c
OLDodin <odin_sasa@mail.ru>
2025-04-05 15:28:21 +0300

    Правки под 16.1

--- Scripts/!sources/EffectOffBG.lua
+++ Scripts/!sources/EffectOffBG.lua
@@ -23,7 +23,7 @@ function onAOPanelLeftClick( params )
 end
 
 function onAOPanelChange( params )
-	if params.unloading and string.find(params.name, "AOPanel") then
+	if params.state == ADDON_STATE_NOT_LOADED and string.find(params.name, "AOPanel") then
 		DnD.ShowWdg(getChild(mainForm, "EFButton"))
 	end
 end
